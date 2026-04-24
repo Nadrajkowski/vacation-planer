3e845f09df1074d2253f87783f6eb170332f9d7d
Kasper Nadrajkowski <kasper@bluestonepim.com>
2026-04-24 16:10:13 +0200

    add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vacation-planer/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
